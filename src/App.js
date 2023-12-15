0cb0eb6b00c1e9ae2117ff687a4e7d6ddd7c425b
kentrung <trungnt256@gmail.com>
2023-12-16 02:12:06 +0700

    Initial project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { mockData } from "./mockData";
 
-function App() {
+export default function App() {
+  const [provinces, setProvinces] = useState([]);
+  const [dataInfo, setDataInfo] = useState({
+    selectedCity: "",
+    isSelectedCity: false,
+    selectedProvince: "",
+    isSelectedProvince: false,
+  });
+
+  const handleChangeCity = (event) => {
+    const selectedCity = event.target.value;
+    if (selectedCity === "") {
+      setProvinces([]);
+      setDataInfo({
+        selectedCity: "",
+        isSelectedCity: false,
+        selectedProvince: "",
+        isSelectedProvince: false,
+      });
+      return;
+    }
+    const filterCity = mockData.find((item) => item.id === selectedCity);
+    const filterProvince = filterCity.province;
+    setProvinces(filterProvince);
+    setDataInfo({
+      selectedCity,
+      isSelectedCity: true,
+      selectedProvince: "",
+      isSelectedProvince: false,
+    });
+  };
+
+  const handleChangeProvince = (event) => {
+    const selectedProvince = event.target.value;
+    if (selectedProvince === "") {
+      setDataInfo({
+        ...dataInfo,
+        selectedProvince: "",
+        isSelectedProvince: false,
+      });
+    } else {
+      setDataInfo({ ...dataInfo, selectedProvince, isSelectedProvince: true });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <h2>Chọn thành phố</h2>
+      <select className="form-select mb-3" onChange={handleChangeCity}>
+        <option value="">Chọn thành phố</option>
+        {mockData.map((city) => (
+          <option key={city.id} value={city.id}>
+            {city.name}
+          </option>
+        ))}
+      </select>
+      <h2>Chọn quận</h2>
+      <select className="form-select mb-3" onChange={handleChangeProvince}>
+        <option value="">Chọn quận</option>
+        {provinces.map((province) => (
+          <option key={province.id} value={province.id}>
+            {province.name}
+          </option>
+        ))}
+      </select>
+      <button
+        className="btn btn-primary"
+        disabled={!dataInfo.isSelectedCity || !dataInfo.isSelectedProvince}
+        onClick={() =>
+          alert(
+            JSON.stringify({
+              "Thành Phố": dataInfo.selectedCity,
+              Quận: dataInfo.selectedProvince,
+            })
+          )
+        }
+      >
+        Submit
+      </button>
     </div>
   );
 }
-
-export default App;
---
+++ src/mockData.js
@@ -0,0 +1,29 @@
+export const mockData = [
+  {
+    id: "hanoi",
+    name: "Hà Nội",
+    province: [
+      { id: "hoankiem", name: "Hoàn Kiếm" },
+      { id: "haibatrung", name: "Hai Bà Trưng" },
+      { id: "hoangmai", name: "Hoàng Mai" }
+    ]
+  },
+  {
+    id: "danang",
+    name: "Đà Nẵng",
+    province: [
+      { id: "haichau", name: "Hải Châu" },
+      { id: "camle", name: "Cẩm Lệ" },
+      { id: "thanhkhe", name: "Thanh Khê" }
+    ]
+  },
+  {
+    id: "saigon",
+    name: "Sài Gòn",
+    province: [
+      { id: "tanbinh", name: "Tân Bình" },
+      { id: "tanphu", name: "Tân Phú" },
+      { id: "phunhuan", name: "Phú Nhuận" }
+    ]
+  }
+];
